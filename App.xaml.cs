4bfcdeb4e16bbd6196eb0b01e7ae8a2208333452
pc <pc@DESKTOP-0OPNLBJ>
2020-10-31 22:38:53 -0400

    Examen

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RaldyP2Apli
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Ha ocurrido un error {e.Exception.Message}", "");
+            e.Handled = true;
+        }
+
+    }
+}
